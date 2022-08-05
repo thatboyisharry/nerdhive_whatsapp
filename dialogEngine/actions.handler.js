5d237ec3a2efa810de8150c0eebe10dfde34344e
Glitch (messenger-platform-quick-start) <none>
2022-08-05 12:09:13 +0000

    🍖🏪 Checkpoint ./dialogEngine/actions.handler.js:46558660/2293

--- dialogEngine/actions.handler.js
+++ dialogEngine/actions.handler.js
@@ -1,21 +1,38 @@
 const { onboardingActions } = require('../flows/onboarding/actions');
 const { landlordFlowActions } = require('../flows/landlord/actions');
-const actionsHandler= async(transition,user_response,user)=>{
+const actionsHandler= async(node,user_response,user)=>{
   
-  let executed;
-  if(transition.flow==='onboarding'){
-    console.log("onboading flow...")
-    executed = await onboardingActions(transition.actions,user_response,user);
+  if(node.actions>0){
+    let action=node.actions[0]
+  
+    if(node.actions.length>1){
+      for(let i = 0;i<node.actions.length;i++){
+        let node_action = node.actions[i];
+        if(node_action.trigger==='user_response'){
+              action=node_action
+        }
+      }
+    }
+    
+    let executed;
+    if(action.flow==='onboarding'){
+      console.log("onboading flow...")
+      executed = await onboardingActions(action.name,user_response,user);
+    }
+    if(action.flow==='landlord'){
+      console.log("landlord flow...")
+      console.log(action.name)
+      executed = await landlordFlowActions(action.name,user_response,user);
+    }
+
+    return true   
+    
+    
   }
-  if(transition.flow==='landlord'){
-    console.log("landlord flow...")
-    console.log(transition.actions)
-    executed = await landlordFlowActions(transition.actions,user_response,user);
-  }
   
-  
- 
   return true
+  
+  
   
   
 }
