e34f71cb49a627f90ff8eb068d8337c0617b28b0
Glitch (messenger-platform-quick-start) <none>
2022-08-08 19:06:17 +0000

    💇👍 Checkpoint ./models/interactiveUI.model.js:46558660/1747

--- models/interactiveUI.model.js
+++ models/interactiveUI.model.js
@@ -3,27 +3,79 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const headerIDMeta = new Schema(
+
+
+const HeaderMetaSchema = new Schema(
     {
+      
         id:{
             type:String,
         },
+        link:{
+            type:String,
+        },
+        provider:{
+            name:{
+              type:String
+            }
+        },
         filename:{
-            type:String
+          type:String
         }
     }
 )
 
-const documentHeader = new Schema(
+const DocumentHeaderSchema = new Schema(
     {
         type:{
             type:String,
             default:'document'
         },
         document:{
-            type:String
+            type:HeaderMetaSchema
         }
     }
 )
+
+
+const VideoHeaderSchema = new Schema(
+    {
+        type:{
+            type:String,
+            default:'video'
+        },
+        video:{
+            type:HeaderMetaSchema
+        }
+    }
+)
+
+
+const ImageHeaderSchema = new Schema(
+    {
+        type:{
+            type:String,
+            default:'image'
+        },
+        image:{
+            type:HeaderMetaSchema
+        }
+    }
+)
+
+const TextHeaderSchema = new Schema(
+    {
+        type:{
+            type:String,
+            default:'text'
+        },
+        text:{
+            type:HeaderMetaSchema
+        }
+    }
+)
+
+
+
 
 module.exports = mongoose.model("Flow", FlowSchema);
