cfd5d2a3927d6222c264e8afb2ba09716f37d913
Glitch (messenger-platform-quick-start) <none>
2022-09-10 01:24:10 +0000

    🍣😼 Checkpoint ./dialogEngine/index.js:46558660/7

--- dialogEngine/index.js
+++ dialogEngine/index.js
@@ -2,7 +2,7 @@ const {sendResponse,updateStatus} = require("./sendResponse");
 const {getUserResponse} = require("./getUserResponse");
 const {getBotResponses} = require("./getBotResponse");
 const { getProject} = require('../services/apiCalls');
-const PROJECT_NAME='KasiRentals'
+const PROJECT_NAME='kasi rentals'
 
 const Project= getProject(PROJECT_NAME);
 
