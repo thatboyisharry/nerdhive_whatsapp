b1cd8973e7973f0ce43d9dd66fa2fbc353226487
Glitch (messenger-platform-quick-start) <none>
2022-10-05 13:55:20 +0200

    template actions additions reschdule flow

--- messages/templates.js
+++ messages/templates.js
@@ -1,4 +1,4 @@
-const {getProperty, getTimetable, getLesson} = require("../services/apiCalls");
+const {getProperty, getTimetable, getLesson, getTutor, getLearner} = require("../services/apiCalls");
 const { getWeelkyAppointments } = require("./utils");
 
 
@@ -22,7 +22,7 @@ const templateActionsHandler=async(message,user)=>{
       }
       //fill templates with lesson details and return an array of templates
       for(let i = 0 ; i < lessons.length; i++){
-        filledTemplate = await  upcomingLessonsActions(template,lessons[i])
+        filledTemplate = await  upcomingLessonsTemplate(template,lessons[i])
         message.template=filledTemplate
         messages.push(message);
       }
@@ -30,6 +30,21 @@ const templateActionsHandler=async(message,user)=>{
       
       return messages
 
+    }
+
+    
+    if(template.name==='confirm_reschedule_lesson'){
+      
+      filledTemplate = await confirmRescheduleTemplate(template,user)
+      message.template = filledTemplate;
+      return message;
+    }
+
+    if(template.name==='confirm_time_changes'){
+      
+      filledTemplate = await confirmTimeChangesTemplate(template,user)
+      message.template = filledTemplate;
+      return message;
     }
 
     // if(template.name==='landlord_or_renter'){
@@ -41,7 +56,7 @@ const templateActionsHandler=async(message,user)=>{
 
 }
 
-const upcomingLessonsActions=async(template,lesson)=>{
+const upcomingLessonsTemplate=async(template,lesson)=>{
 
     template.components[0].parameters[0].text=lesson.code;
     template.components[1].parameters[0].text=lesson.topic;
@@ -52,6 +67,55 @@ const upcomingLessonsActions=async(template,lesson)=>{
 
 }
 
+const confirmRescheduleTemplate=async(template,user)=>{
+  const { lesson } = user.session.data
+
+  if(user.isLearner){
+    let tutor = await getTutor(lesson.tutorId);
+    template.components[0].parameters[0].text=tutor.name;
+    template.components[0].parameters[1].text=lesson.day;
+    template.components[0].parameters[2].text=lesson.time;
+  }
+  //Reschedule lesson with {tutor} on {day} @ {time} 
+  if(user.isTutor){
+    let learner = await getLearner(lesson.learnerId)
+    template.components[0].parameters[0].text=learner.name;
+    template.components[0].parameters[1].text=lesson.day;
+    template.components[0].parameters[2].text=lesson.time;
+  }
+ 
+
+return template
+
+}
+
+const confirmTimeChangesTemplate=async(template,user)=>{
+  const { lesson, appointment} = user.session.data
+
+  if(user.isLearner){
+    let tutor = await getTutor(lesson.tutorId);
+    template.components[0].parameters[0].text=tutor.name;
+    template.components[0].parameters[1].text=lesson.day;
+    template.components[0].parameters[2].text=lesson.time;
+    template.components[0].parameters[2].text=appointment.day;
+    template.components[0].parameters[4].text=appointment.time;
+    //you  have changed lesson with {tutor} from {day} @ {time} to {day} @time
+  }
+  
+  if(user.isTutor){
+    let learner = await getLearner(lesson.learnerId)
+    template.components[0].parameters[0].text=learner.name;
+    template.components[0].parameters[1].text=lesson.day;
+    template.components[0].parameters[2].text=lesson.time;
+    template.components[0].parameters[3].text=appointment.day;
+    template.components[0].parameters[4].text=appointment.time;
+    //you  have changed lesson with {learner} from {day} @ {time} to {day} @time
+  }
+ 
+
+return template
+
+}
 // const landlordOrRenter_Actions = async(template,user)=>{
     
 //     template.components[0].parameters[0].text=user.name;
